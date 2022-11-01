576ce2ec0f0bf8801eab8d46805fa2876e3d1f0e
Student <student@epicodus-4.hsd1.or.comcast.net>
2022-11-01 10:26:10 -0700

    update ui logic
    co-authored-by: Yoonis Ali <yoonismahamoudali@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,57 @@ function AddressBook() {
   this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 };
 
-AddressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 };
 
-AddressBook.prototype.findContact = function(id) {
+AddressBook.prototype.findContact = function (id) {
   if (this.contacts[id] !== undefined) {
     return this.contacts[id];
   }
   return false;
 };
 
-AddressBook.prototype.deleteContact = function(id) {
+AddressBook.prototype.deleteContact = function (id) {
   if (this.contacts[id] === undefined) {
     return false;
   }
-    delete this.contacts[id];
-    return true;
+  delete this.contacts[id];
+  return true;
 };
 
 // Business Logic for Contacts ---------------------------
-function Contact(firstName, lastName, phoneNumber) {
+function Address(emailAddress, homeAddress) {
+  let address = new Address(emailAddress, homeAddress);
+  this.emailAddress = emailAddress;
+  this.homeAddress = homeAddress;
+  return address;
+}
+
+function Contact(firstName, lastName, phoneNumber, address) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.address = address;
 }
 
-Contact.prototype.fullName = function() {
+Address.prototype.fullAddress = function () {
+  return this.emailAddress + " " + this.homeAddress;
+}
+
+Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 };
+
+Contact.prototype.fullContact = function () {
+  return this.firstName + " " + this.lastName + " " + this.phoneNumber + " " + this.address;
+}
 
 //UI Logic-------------------------------------------------------
 let addressBook = new AddressBook();
@@ -48,10 +64,10 @@ function listContacts(addressBookToDisplay) {
   let contactsDiv = document.querySelector("div#contacts");
   contactsDiv.innerText = null;
   const ul = document.createElement("ul");
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
     const contact = addressBookToDisplay.findContact(key);
     const li = document.createElement("li");
-    li.append(contact.fullName());
+    li.append(contact.fullContact());
     li.setAttribute("id", contact.id);
     ul.append(li);
   });
@@ -79,15 +95,19 @@ function handleFormSubmission(event) {
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  const inputtedEmail = document.querySelector("input#new-email-address").value;
+  const inputtedHomeAddress = document.querySelector("input#new-home-address").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedHomeAddress);
   addressBook.addContact(newContact);
   listContacts(addressBook);
   document.querySelector("input#new-first-name").value = null;
   document.querySelector("input#new-last-name").value = null;
   document.querySelector("input#new-phone-number").value = null;
+  document.querySelector("input#new-email-address").value = null;
+  document.querySelector("input#new-home-address").value = null;
 }
 
-window.addEventListener("load", function (){
+window.addEventListener("load", function () {
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
   document.querySelector("div#contacts").addEventListener("click", displayContactDetails);
   document.querySelector("button.delete").addEventListener("click", handleDelete);
